2ce58bab6da40cca9d26f2acbafb634d71e4117c
Tung Vuduc <tungvuduc@Tungs-MacBook-Pro.local>
2023-05-25 14:07:01 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+# Call setup function
+setup(
+    author="Vu Duc Tung",
+    description="A package for downstream data analysis of proteomics data",
+    name="proteomics_downsream_analysis",
+    version="0.1.0",
+)
