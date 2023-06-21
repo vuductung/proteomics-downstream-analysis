f7388500f72e584bf67e7dff45300abbcf6a7fc3
Tung Vuduc <tungvuduc@Tungs-MacBook-Pro.local>
2023-06-21 15:07:46 +0200

    delete caches

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email="tungvuduc@outlook.de",
     description="A package for downstream data analysis of proteomics data",
     keywords = ["proteomics", "downstream analysis", "data analysis", "data visualization", "mass spectrometry"],
+    version="0.1.0",
     url="https://github.com/vuductung/proteomics-downstream-anlaysis",
     packages=find_packages(include=["proteomics_downstream_analysis", "proteomics_downstream_analysis.*"]),
     python_requires="3.11.3",
@@ -30,7 +31,5 @@ setup(
                     "umap",
                     "UpSetPlot",
                     "venn"
-                    ],
-    use_scm_version=True,
-    setup_requires=['setuptools_scm']
+                    ]
 )
