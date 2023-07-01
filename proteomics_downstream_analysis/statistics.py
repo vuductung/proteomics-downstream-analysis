e5fb061774b3f7ed2170e6136e6392a7933f1dab
Tung Vuduc <tungvuduc@Tungs-MacBook-Pro.local>
2023-07-01 14:04:51 +0200

    edits

--- proteomics_downstream_analysis/statistics.py
+++ proteomics_downstream_analysis/statistics.py
@@ -31,30 +31,72 @@ class Statistics:
         fc_data = pd.DataFrame()
         pv_data = pd.DataFrame()
         qv_data = pd.DataFrame()
-        
+        t_stat_data = pd.DataFrame()
+        ci_data = pd.DataFrame()
+        cohensd_data = pd.DataFrame()
+
         for a, b in comparisons:
             
-
-            a_data = self.data[a].to_numpy()
-            b_data = self.data[b].to_numpy()
-
             # T test for each row pair.
-            pvalues = stats.ttest_ind(a=a_data, b=b_data, axis=1)[1]
+            t_stats, pvalues = stats.ttest_ind(a=self.data[a], b=self.data[b], axis=1)
+
+            t_stat_data[f'{a}/{b}'] = t_stats
 
             pv_data[f'{a}/{b}'] = - np.log10(pvalues)
 
             # Mean difference for each row.
-            fc_data[f'{a}/{b}'] = np.mean(a_data, axis=1) - np.mean(b_data, axis=1)
+            fc_data[f'{a}/{b}'] = self._fold_change(self.data[a], self.data[b])
+            
+            # 95% confidence interval for each row.
+            ci_data[f'{a}/{b}'] = self._ttest_conf_int(self.data[a], self.data[b])
+
+            # Cohens d for each row.
+            cohensd_data[f'{a}/{b}'] = self._cohensd(self.data[a], self.data[b])
 
             # bh adjusted pvalue 
             qv_data[f'{a}/{b}'] = fdrcorrection(pvalues)[1]
-        
+
         self.fc_data = self.data.select_dtypes('string').merge(fc_data, left_index = True, right_index = True)
         self.pv_data = self.data.select_dtypes('string').merge(pv_data, left_index = True, right_index = True)
         self.qv_data = self.data.select_dtypes('string').merge(qv_data, left_index = True, right_index = True)
-        
+        self.t_stat_data = self.data.select_dtypes('string').merge(t_stat_data, left_index = True, right_index = True)
+        self.ci_data = self.data.select_dtypes('string').merge(ci_data, left_index = True, right_index = True)
+        self.cohensd_data = self.data.select_dtypes('string').merge(cohensd_data, left_index = True, right_index = True)
+
         if return_output == True:
-            return self.fc_data, self.pv_data, self.qv_data
+            return self.fc_data, self.pv_data, self.qv_data, self.t_stat_data, self.ci_data, self.cohensd_data
+    
+    def _ttest_conf_int(self, a, b, conf_int=0.95):
+
+        sd_a = np.std(a, ddof=1, axis=1)
+        sd_b = np.std(b, ddof=1, axis=1)
+
+        n_a = a.shape[1]
+        n_b = b.shape[1]
+        df = n_a + n_b - 2
+        s_pooled = np.sqrt(((n_a-1)*sd_a**2 + (n_b-1)*sd_b**2) / df)
+
+        percentile = (1 + conf_int)/2
+        t_crit = stats.t.ppf(percentile, df)
+        lower = self._fold_change(a, b) - t_crit * s_pooled
+        upper = self._fold_change(a, b) + t_crit * s_pooled
+
+        # Combine lower with upper
+        conf_intervals = list(zip(lower, upper))
+        return conf_intervals
+
+    def _fold_change(self, a, b):
+
+        fold_change = np.mean(a, axis=1) - np.mean(b, axis=1)
+        return fold_change
+    
+    def _cohensd(self, a, b):
+
+        n1, n2 = a.shape[1], b.shape[1]
+        s1, s2 = np.std(a, ddof=1, axis=1), np.std(b, ddof=1, axis=1)
+        s_pooled = np.sqrt(((n1-1)*s1 + (n2-1)*s2) / (n1 + n2 - 2))
+
+        return (self._fold_change(a, b)) / s_pooled
     
     def get_unique_comb(self, a, b):
 
