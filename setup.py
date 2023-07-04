bf16703147facec32c8ef4e6230501c06ffa4d68
Tung Vuduc <tungvuduc@Tungs-MacBook-Pro.local>
2023-07-04 21:00:49 +0200

    edits

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords = ["proteomics", "downstream analysis", "data analysis", "data visualization", "mass spectrometry"],
-    version="0.1.1",
+    version="0.1.2",
     url="https://github.com/vuductung/proteomics-downstream-anlaysis",
     packages=find_packages(include=["repos","proteomics_downstream_analysis", "proteomics_downstream_analysis.*"]),
     python_requires=">=3.6.1",
